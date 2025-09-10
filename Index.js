9ca9b5f9d3068cc5cc0a6edbe4a79bf852f1b109
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-09-10 15:28:19 +0200

    Delete Index.js

--- Index.js
+++
@@ -1,7 +0,0 @@
-// Mobile menu toggle
-const mobileToggle = document.getElementById("mobile-toggle");
-const navMenu = document.querySelector(".nav");
-
-mobileToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("hidden");
-});
