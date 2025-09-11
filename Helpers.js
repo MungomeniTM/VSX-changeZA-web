c7cc9f3c0aaa9c7173caf513f66773700931143b
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-09-11 19:12:39 +0200

    Delete Helpers.js

--- Helpers.js
+++
@@ -1,7 +0,0 @@
-// utils/helpers.js
-window.helpers = (function(){
-  function formatTimeISO(iso) {
-    try { return new Date(iso).toLocaleString(); } catch(e) { return iso; }
-  }
-  return { formatTimeISO };
-})();
