b64b59ba956c4af8c3726ea343fa392809c41a68
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-09-17 18:08:44 +0200

    Update user.py

--- schemas/user.py
+++ schemas/user.py
@@ -1,18 +1,19 @@
 from pydantic import BaseModel, EmailStr
 
-class UserCreate(BaseModel):
+class UserBase(BaseModel):
     name: str
     email: EmailStr
+    role: str
+
+class UserCreate(UserBase):
     password: str
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-class UserResponse(BaseModel):
+class UserResponse(UserBase):
     id: int
-    name: str
-    email: EmailStr
 
     class Config:
         orm_mode = True
