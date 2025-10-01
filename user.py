b463889755a5a50b13ff777e69258d1f1b148a17
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-10-01 18:49:52 +0200

    Update user.py

--- user.py
+++ user.py
@@ -9,9 +9,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, default="client")  # client, farmer, skilled
-    from fastapi.staticfiles import StaticFiles
-from app.routes import auth, users, uploads
-from app.core.config import API_PREFIX, UPLOAD_DIR
+    
 
 if payload.photos is not None: update_values['photos'] = json.dumps(payload.photos)
 if payload.companies is not None: update_values['companies'] = json.dumps(payload.companies)
