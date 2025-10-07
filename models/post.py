bd79582e89abe2f895aa37b44ab481d97c6ecfce
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-10-07 15:07:01 +0200

    Update post.py

--- models/post.py
+++ models/post.py
@@ -1,18 +1,18 @@
-# app/models/post.py
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app.core.database import Base
+
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, index=True)
-    text = Column(String, nullable=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    text = Column(Text, nullable=True)
     media = Column(String, nullable=True)
     media_type = Column(String, nullable=True)
     approvals = Column(Integer, default=0)
     shares = Column(Integer, default=0)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User", back_populates="posts")
+    user = relationship("User")
