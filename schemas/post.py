8742c74ee70acc4543f3887f71951dfd099e77d5
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-10-07 14:53:58 +0200

    Update post.py

--- schemas/post.py
+++ schemas/post.py
@@ -1,25 +1,21 @@
+# app/schemas/post.py
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List
-from app.schemas.user import UserOut
+from typing import Optional
 
-class CommentOut(BaseModel):
-    id: int
-    text: str
-    user: UserOut | None
-    created_at: datetime
-    class Config:
-        orm_mode = True
+class PostBase(BaseModel):
+    text: Optional[str] = None
+    media: Optional[str] = None
 
-class PostOut(BaseModel):
+class PostCreate(PostBase):
+    pass
+
+class PostOut(PostBase):
     id: int
-    text: str | None
-    media: str | None
-    media_type: str | None
+    media_type: Optional[str]
     approvals: int
     shares: int
     created_at: datetime
-    user: UserOut
-    comments: List[CommentOut] = []
+
     class Config:
         orm_mode = True
