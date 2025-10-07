91c5f2f597a0bdef596f88a70250291101104bdd
Tshidzula Mulisa Mungomeni <mulisa405@gmail.com>
2025-10-07 16:05:17 +0200

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,3 +1,4 @@
+# backend/app/core/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,7 +8,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
